f448c2d598925e630748d598a6715c952b3f5eef
JoaquinGianan <90005176+JoaquinGianan@users.noreply.github.com>
2023-03-11 17:03:43 -0500

    Intitializing new practice folder

---
+++ README.md
@@ -0,0 +1,1 @@
+Project that contains training threads
---
+++ iterators.py
@@ -0,0 +1,60 @@
+
+
+class SequenceIterator:
+    def __init__(self, sequence):
+        self._sequence = sequence
+        self._index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._index < len(self._sequence):
+            item = self._sequence[self._index]
+            self._index += 1
+            return item
+        else:
+            raise StopIteration
+        
+
+class SquareIterator:
+    def __init__(self, sequence):
+        self._sequence = sequence
+        self._index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._index < len(self._sequence):
+            square = self._sequence[self._index] ** 2
+            self._index += 1
+            return square
+        else:
+            raise StopIteration
+
+
+
+class FibonacciIterator:
+    def __init__(self, stop=10):
+        self._stop = stop
+        self._index = 0
+        self._current = 0
+        self._next = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._index < self._stop:
+            self._index += 1
+            fib_number = self._current
+            self._current, self._next = (
+                self._next,
+                self._current + self._next,
+            )
+            return fib_number
+        else:
+            raise StopIteration
+
+
